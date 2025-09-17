1047b56a0c1bcc19bdf42558a1cdf9460512c716
Vartikajoshi31 <148364714+Vartikajoshi31@users.noreply.github.com>
2025-09-17 18:37:54 +0000

    conversion of data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accId = 14453
+let accEmail="joshivartika0922gmail.com"
+var accPassword="12345"
+accCity = "Jaipur"
+
+//accId=2
+
+console.log(accId);
+/*
+dont use var because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";  //treat all js code as newer version
+
+alert("hello")
+
+//alert(3+3) //we are using nodejs,not browser
+
+//number = 2 to power 53 
+//bigInt
+//string =single and double quote both
+//boolean=true and false
+//null=standalone value(representation of empty value)//let state=null
+//undefined=value is not assigned yet
+//symbol=unique
+
+
+//object
+console.log(typeof "vartika");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
